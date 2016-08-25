a6536e1dd7308aee99de0cd165506f52f8e14fc3
Zuko09 <varunknath@gmail.com>
2016-08-25 16:08:49 -0700

    Added Roster Functionality

--- humpsub-website/js/angular.js
+++ humpsub-website/js/angular.js
@@ -1,97 +1,86 @@
 var app1 = angular.module('app1', []);
 
-app1.controller('ctrl1', function($scope) {
-    $scope.title = "The Team";
+app1.controller('ctrl1', function($scope, $window) {
+
     $scope.roster = [
         subteam1 = {
             name : 'Hull Team',
             members : [
-              {
-                name : 'Jack Gentsch'
-              },
-              {
-                name : 'Joe Joe Jimenz'
-              },
-              {
-                name : 'Aman Arya'
-              },
-              {
-                name : 'Connor Moen'
-              }
+              {name : 'Jack Gentsch', link: "https://www.linkedin.com"},
+              {name : 'Joe Joe Jimenz', link: "https://www.linkedin.com"},
+              {name : 'Aman Arya', link: "https://www.linkedin.com"},
+              {name : 'Connor Moen', link: "https://www.linkedin.com"},
+              {name : 'Connor Moen', link: "https://www.linkedin.com"},
+              {name : 'Connor Moen', link: "https://www.linkedin.com"},
+              {name : 'Connor Moen', link: "https://www.linkedin.com"},
+              {name : 'Connor Moen', link: "https://www.linkedin.com"},
+              {name : 'Connor Moen', link: "https://www.linkedin.com"},
+              {name : 'Connor Moen', link: "https://www.linkedin.com"},
+              {name : 'Connor Moen', link: "https://www.linkedin.com"},
+              {name : 'Connor Moen', link: "https://www.linkedin.com"},
+              {name : 'Connor Moen', link: "https://www.linkedin.com"}
             ]
         },
         subteam2 = {
             name : 'Fin Team',
             members : [
-              {
-                name : 'Jack Gentsch'
-              },
-              {
-                name : 'Joe Joe Jimenz'
-              },
-              {
-                name : 'Aman Arya'
-              },
-              {
-                name : 'Connor Moen'
-              }
+              {name : 'Jack Gentsch', link: "https://www.linkedin.com"},
+              {name : 'Joe Joe Jimenz', link: "https://www.linkedin.com"},
+              {name : 'Aman Arya', link: "https://www.linkedin.com"},
+              {name : 'Connor Moen', link: "https://www.linkedin.com"}
             ]
         },
         subteam3 = {
             name : 'Electronics Team',
             members : [
-              {
-                name : 'Jack Gentsch'
-              },
-              {
-                name : 'Joe Joe Jimenz'
-              },
-              {
-                name : 'Aman Arya'
-              },
-              {
-                name : 'Connor Moen'
-              }
+              {name : 'Jack Gentsch', link: "https://www.linkedin.com"},
+              {name : 'Joe Joe Jimenz', link: "https://www.linkedin.com"},
+              {name : 'Aman Arya', link: "https://www.linkedin.com"},
+              {name : 'Connor Moen', link: "https://www.linkedin.com"}
             ]
         },
         subteam4 = {
             name : 'Human Team',
             members : [
-              {
-                name : 'Jack Gentsch'
-              },
-              {
-                name : 'Joe Joe Jimenz'
-              },
-              {
-                name : 'Aman Arya'
-              },
-              {
-                name : 'Connor Moen'
-              }
+              {name : 'Jack Gentsch', link: "https://www.linkedin.com"},
+              {name : 'Joe Joe Jimenz', link: "https://www.linkedin.com"},
+              {name : 'Aman Arya', link: "https://www.linkedin.com"},
+              {name : 'Connor Moen', link: "https://www.linkedin.com"}
             ]
         }
     ];
-
+    $scope.title = "The Team";
     $scope.display = $scope.roster;
+    $scope.is_showing_members = false;
+    $scope.current_section = -1;
 
     $scope.reset = function() {
+        var temp = [];
         for(var i=0; i < $scope.roster.length; i++) {
-            $scope.display[i] = $scope.roster[i];
+            temp[i] = $scope.roster[i];
         }
+        $scope.display = temp;
+        $scope.title = "The Team";
+        $scope.is_showing_members = false;
+        $scope.current_section = -1;
     };
 
     $scope.reset();
 
-    $scope.show_subsection = function(section){
-        var len = $scope.roster[section].members.length;
-        console.log("roster"+$scope.roster);
-        for(var i=0; i < len; i++) {
-            $scope.display[i] = $scope.roster[section].members[i];
-            console.log($scope.roster[0].members);
-            console.log($scope.display);
+    $scope.show_next = function(index, memeber){
+        if($scope.is_showing_members == false) {
+            var len = $scope.roster[index].members.length;
+            var temp = [];
+            console.log("roster"+$scope.roster);
+            for(var i=0; i < len; i++) {
+                temp[i] = $scope.roster[index].members[i];
+            }
+            $scope.title = $scope.roster[index].name;
+            $scope.display = temp;
+            $scope.is_showing_members = true;
+        } else {
+            $window.location.href = memeber.link;
         }
-
     };
 
 });
